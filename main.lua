4a7a97794c351a710494d6583f46ec07d52defec
Derek Miranda <derek.miranda@hearst.com>
2025-03-20 14:09:26 -0400

    add btn + some layout

--- main.lua
+++ main.lua
@@ -6,22 +6,26 @@ local aliveClr = {0.75, 0.75, 0.75}
 local aliveHoverClr = {0.6, 0.6, 0.6}
 
 -- # of cells, horizontally + vertically, in grid
-local gridCellWt = 24
-local gridCellHt = 24
+local gridCellWt = 16
+local gridCellHt = 16
 
 -- grid rendering properties
 local cellLn = 16
 local cellGap = 4
+local gridX = love.graphics.getWidth() / 12
+local gridY = 10 * love.graphics.getHeight() / 12 - gridCellHt * cellLn - (gridCellHt - 1) * cellGap
 
+-- refs
 local grid = nil
+local btns = {}
 
 function initializeGrid()
     grid = {} -- create the matrix
     for i = 1, gridCellWt do
         grid[i] = {} -- create a new row
         for j = 1, gridCellHt do
-            local cellX = i * cellLn + (i - 1) * cellGap
-            local cellY = j * cellLn + (j - 1) * cellGap
+            local cellX = gridX + (i - 1) * cellLn + (i - 1) * cellGap
+            local cellY = gridY + (j - 1) * cellLn + (j - 1) * cellGap
             grid[i][j] = ui.createCell({
                 x = cellX,
                 y = cellY,
@@ -36,6 +40,19 @@ function initializeGrid()
     end
 end
 
+function initializeBtns()
+    btns.advance = ui.createButton({
+        text = "ADVANCE",
+        x = gridX,
+        y = 10 * love.graphics.getHeight() / 12 + 2 * cellGap,
+        w = 80,
+        h = cellLn,
+        color = aliveClr,
+        hoverColor = aliveHoverClr,
+        pressedColor = deadHoverClr
+    })
+end
+
 function forEachCell(cb)
     for i, row in pairs(grid) do
         for j, cell in pairs(row) do
@@ -45,6 +62,7 @@ function forEachCell(cb)
 end
 
 function love.load()
+    initializeBtns()
     initializeGrid()
 end
 
@@ -52,18 +70,27 @@ function love.mousemoved(x, y, dx, dy, isTouch)
     forEachCell(function(cell)
         cell.mousemoved(x, y)
     end)
+    for _, btn in pairs(btns) do
+        btn.mousemoved(x, y)
+    end
 end
 
-function love.mousepressed(x, y, btn)
+function love.mousepressed(x, y, mouseBtn)
     forEachCell(function(cell)
-        if btn == 1 then
+        if mouseBtn == 1 then
             cell.toggle()
         end
     end)
+    for _, btn in pairs(btns) do
+        btn.mousepressed(mouseBtn)
+    end
 end
 
 function love.draw()
     forEachCell(function(cell)
         cell.draw()
     end)
+    for _, btn in pairs(btns) do
+        btn.draw()
+    end
 end
--- ui.lua
+++ ui.lua
@@ -1,4 +1,49 @@
 local module = {}
+local noop = function()
+end
+
+function resetColor()
+    love.graphics.setColor(love.graphics.getBackgroundColor())
+end
+
+function module.createButton(args)
+    local x = args.x
+    local y = args.y
+    local w = args.w
+    local h = args.h
+    local text = args.text or ""
+    local color = args.color
+    local hoverColor = args.hoverColor
+    local pressedColor = args.pressedColor
+    local pressedColor = args.pressedColor
+    local onpress = args.onpress or noop
+
+    local btn = {}
+    btn.hovered = false
+    btn.pressed = false
+
+    function btn.draw()
+        local color = btn.hovered and hoverColor or btn.pressed and pressedColor or color
+        love.graphics.setColor(color)
+        love.graphics.rectangle("line", x, y, w, h)
+        love.graphics.print(text, x, y)
+        resetColor()
+    end
+
+    function btn.mousemoved(mx, my)
+        btn.hovered = mx >= x and mx <= x + w and my >= y and my <= y + h
+    end
+
+    function btn.mousepressed(mx, my, mouseBtn)
+        if not btn.hovered then
+            return
+        end
+
+        onpress()
+    end
+
+    return btn
+end
 
 function module.createCell(cellArgs)
     local x = cellArgs.x
@@ -20,7 +65,7 @@ function module.createCell(cellArgs)
                           (cell.alive and aliveColor or deadColor)
         love.graphics.setColor(color)
         love.graphics.rectangle("fill", x, y, w, h)
-        love.graphics.setColor(love.graphics.getBackgroundColor())
+        resetColor()
     end
 
     function cell.mousemoved(mx, my)
